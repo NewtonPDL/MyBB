cf140074934800baaf88a48df164264c3426d1bd
NewtonPDL <modnewton@gmail.com>
2014-09-24 22:47:32 -0300

    Update contact.lang.php

--- 18/inc/languages/ptbr/contact.lang.php
+++ 18/inc/languages/ptbr/contact.lang.php
@@ -3,6 +3,7 @@
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
  * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
+ * Revisado por "NewtonPDL" "http://community.mybb.com/user-35908.html"
  */
 
 $l['contact'] = 'Contate-nos';
@@ -15,7 +16,7 @@ $l['contact_subject_desc'] = 'Resumo da sua mensagem.';
 $l['contact_message'] = 'Mensagem';
 $l['contact_message_desc'] = 'Descrever seu interesse nesta área em detalhe.';
 $l['contact_email'] = 'Email';
-$l['contact_email_desc'] = 'Digite seu e-mail para que possamos contatá-lo(a) de volta.';
+$l['contact_email_desc'] = 'Digite seu e-mail para que possamos contatá-lo(a) respondê-lo.';
 $l['contact_send'] = 'Enviar';
 $l['image_verification'] = "Imagem de verificação";
 $l['verification_note'] = "Por favor insira o texto contido dentro da imagem na caixa de texto abaixo dele. Este processo é usado para evitar mensagens automatizadas.";
@@ -25,6 +26,6 @@ $l['subject_too_long'] = 'O assunto é muito longo. Por favor, digite um assunto
 $l['message_too_short'] = 'A mensagem é muito curta. Por favor digite uma mensagem mais do que {1} caracteres (atualmente {2}).';
 $l['message_too_long'] = 'A mensagem é muito longa. Por favor digite uma mensagem mais curta do que {1} caracteres (atualmente {2}).';
 
-$l['error_stop_forum_spam_spammer'] = 'Desculpe, seu e-mail ou endereço IP corresponde ao de um spammer conhecido e, portanto, o sua mensagem de contato foi bloqueada.';
+$l['error_stop_forum_spam_spammer'] = 'Desculpe, seu e-mail ou endereço IP corresponde ao de um spammer conhecido e, portanto, a sua mensagem de contato foi bloqueada.';
 $l['error_stop_forum_spam_fetching'] = 'Desculpa, algo deu errado, verificando sua mensagem contra um banco de dados de spammers. Provavelmente o banco de dados não pôde ser acessado. Por favor, tente novamente mais tarde.';
 
